2abe744b1908c950fcdee3c0b50a15c178399366
Korvised YOTSAIY <kvsyotsaiy@gmail.com>
2023-03-15 09:04:42 +0700

    first commit

---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+const config = {
+  tabWidth: 2,
+  semi: false,
+  singleQuote: false,
+  arrowParens: "avoid",
+  plugins: [require.resolve("prettier-plugin-tailwindcss")]
+};
+
+module.exports = config;
